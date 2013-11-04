accd6506aa3e3416bc44e5a75389a713f28d1e21
rojasagustin90@gmail.com <rojasagustin90@gmail.com@9076943b-fdcb-4d67-a66b-d7beff98a4b6>
2013-11-04 00:26:37 +0000

    Clases nuevas  para tener todo mas organizado. PreProcesarDatos todavia no esta andando, es el codigo que hizo jas pero llevado a una "clase"
    git-svn-id: svn://localhost/tmp/tp7506/trunk@20 9076943b-fdcb-4d67-a66b-d7beff98a4b6

---
+++ src/LectorDirectorios.cpp
@@ -0,0 +1,56 @@
+/*
+ * LectorDirectorios.cpp
+ *
+ *  Created on: 03/11/2013
+ *      Author: agu
+ */
+
+#include "LectorDirectorios.h"
+
+LectorDirectorios::LectorDirectorios() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LectorDirectorios::~LectorDirectorios() {
+	// TODO Auto-generated destructor stub
+}
+
+vector<string>& LectorDirectorios::leerDir(const char* ruta){
+
+	// variables
+	DIR *dir = NULL;
+	struct dirent *drnt = NULL;
+	vector<string>*archivos = new vector<string>;
+	const char* puntos = ".";
+	const char* puntos2 = "..";
+
+	dir=opendir(ruta);
+
+	if(dir){
+
+		int i=0;
+
+		while((drnt = readdir(dir))){
+
+			if((strcmp(drnt->d_name,puntos)==0)||((strcmp(drnt->d_name,puntos2)==0))) continue;
+
+			stringstream ss;
+			ss<<drnt->d_name;
+			string aux (ss.str());
+			archivos->push_back(aux);
+			i++;
+	    }
+
+	    closedir(dir);
+
+	}else{
+
+		delete archivos;
+		// devuelve uno vacio
+		vector<string>*archivos = new vector<string>;
+		return *archivos;
+	}
+
+	return *archivos;
+}
