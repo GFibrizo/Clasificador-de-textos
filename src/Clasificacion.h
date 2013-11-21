5ed2e9667dca632322c731588c05de1fda33b786
jasminasf@gmail.com <jasminasf@gmail.com@9076943b-fdcb-4d67-a66b-d7beff98a4b6>
2013-11-21 17:31:58 +0000

    Clasificacion.h y .cpp
    git-svn-id: svn://localhost/tmp/tp7506/trunk@69 9076943b-fdcb-4d67-a66b-d7beff98a4b6

---
+++ src/Clasificacion.h
@@ -0,0 +1,18 @@
+#ifndef CLASIFICACION_H_
+#define CLASIFICACION_H_
+#include <vector>
+#include "Cluster.h"
+#include "Punto.h"
+
+using namespace std;
+
+class Clasificacion{
+	
+private:
+
+public:
+	void Clasificacion::Clasificar( vector<Cluster> lista_de_clusters, Punto* nuevo_punto);
+};	
+
+
+#endif /* CLASIFICACION_H_ */
