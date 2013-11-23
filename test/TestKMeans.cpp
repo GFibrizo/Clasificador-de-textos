05435424070ab389d6f644215d8fb0a5faab7881
rojasagustin90@gmail.com <rojasagustin90@gmail.com@9076943b-fdcb-4d67-a66b-d7beff98a4b6>
2013-11-23 02:14:06 +0000

    git-svn-id: svn://localhost/tmp/tp7506/trunk@78 9076943b-fdcb-4d67-a66b-d7beff98a4b6

--- test/TestKMeans.cpp
+++ test/TestKMeans.cpp
@@ -1,3 +1,19 @@
-class TestKMeans{
+/*
+ * TestKMeans.cpp
+ *
+ *  Created on: 22/11/2013
+ *      Author: agu
+ */
+
+#include "TestKMeans.h"
+using namespace std;
+
+TestKMeans::TestKMeans() {
+	// TODO Auto-generated constructor stub
 
 }
+
+TestKMeans::~TestKMeans() {
+	// TODO Auto-generated destructor stub
+}
+
--- test/TestKMeans.h
+++ test/TestKMeans.h
@@ -1,1 +1,17 @@
+/*
+ * TestKMeans.h
+ *
+ *  Created on: 22/11/2013
+ *      Author: agu
+ */
 
+#ifndef TESTKMEANS_H_
+#define TESTKMEANS_H_
+using namespace std;
+class TestKMeans {
+public:
+	TestKMeans();
+	virtual ~TestKMeans();
+};
+
+#endif /* TESTKMEANS_H_ */
