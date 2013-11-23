81b0a195aaa68e331ab10816ac30a86c5ba9acd4
jasminasf@gmail.com <jasminasf@gmail.com@9076943b-fdcb-4d67-a66b-d7beff98a4b6>
2013-11-23 23:13:34 +0000

    .
    git-svn-id: svn://localhost/tmp/tp7506/trunk@81 9076943b-fdcb-4d67-a66b-d7beff98a4b6

--- main.cpp
+++ main.cpp
@@ -18,7 +18,16 @@ using namespace std;
 int main (int argc, char **argv) {
 
 	PreProcesarDatos* preDatos = new PreProcesarDatos("files/");
+	/*lee los archivos, arma el hash principal y lo guarda como atributo. Arma los hash secundarios y los guarda en archivos.*/
 	preDatos->preProcesarDatos();
+	
+	//2)Tomo muestra de M < N docs random para hacer el clustering: obtengo los numeros de docs. 
+	//3)Para cada uno de los indices llama a LeerDatos(numero_de_archivo) que: abre el archivo, lee cada vector, le saca
+	//el formato. Crea el punto correspondiente al doc y lo devuelve.
+	//4)Dentro de esa muestra se toman sqrt(M.K) puntos; se hace el jerarquico aglomerativo de esos puntos para obtener las
+	//k semillas.
+	//5) Hace K-means de la muestra.
+	//6) Se clasifican los demas documentos (abriendolos de a uno).
 
 	return 0;
 }
